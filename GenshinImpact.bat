8569f60395f92a81f0b23c22b437f7962eaa19a3
lyceris-chan <ghostdrain@outlook.com>
2020-10-08 13:29:56 +0200

    GenshinImpact: remove registry change and launch with realtime priority
    Changes:
    - Remove the code responsible for changing the graphics settings by registry and replace this with a launch option.
    - The script will now require admin privileges which it will acquire on its own (by launching an UAC prompt) to allow the CPU priority to be set (this is needed because the game runs as admin)

--- GenshinImpact.bat
+++ GenshinImpact.bat
@@ -5,49 +5,53 @@ title Lacia's GenshinImpact.bat script
 rem Clear the screen of previous commands
 cls
 
+call :isAdmin
+
+if %errorlevel% == 0 (
+goto :run
+) else (
+echo Requesting administrative privileges...
+goto :UACPrompt
+)
+
+exit /b
+
+:isAdmin
+fsutil dirty query %systemdrive% >nul
+exit /b
+
+:run
 rem Tell users what this script will do.
 echo This script will set the graphics quality to its lowest / the fastest preset and it will launch the game
 
 rem Wait for user input
 pause
 
-rem Set the graphics settings to low with a registery tweak.
-
 rem Clear the screen of previous commands
 cls
-
-echo Setting the graphics quality to low please wait
-reg import lowgraphics.reg
-timeout /t 2 /nobreak > NUL
-
-rem Clear the screen of previous commands
-cls
-
-rem Let the users know that we changed the graphics for them
-
-rem Clear the screen of previous commands
-cls
-
-echo Done setting the graphics preset to fastest.
-timeout /t 2 /nobreak > NUL
-
-rem Wait for user input
-
-rem Clear the screen of previous commands
-cls
-
-pause
-
-rem Clear the screen of previous commands
-cls
-
 
 rem Let the user enter the location where the game is installed
 echo Example: D:\Program Files\Genshin Impact\Genshin Impact Game
 set /p gamelocation="Enter the location where the game is installed: "
 
 
-rem Starting game with -force-d3d11 and launch it in 800x600
+rem Starting game with [-force-d3d11 -screen-width 800 -screen-height 600 -screen-quality lowest] This will run the game on its lowest graphical preset allong with running it in 800x600
 echo Starting the game...
-"%gamelocation%"\GenshinImpact.exe -screen-width 800 -screen-height 600 -force-d3d11
+start "" "%gamelocation%"\GenshinImpact.exe -screen-width 800 -screen-height 600 -force-d3d11 -screen-quality lowest
+
+rem Wait 30 seconds or untill a user presses a key
+timeout /t 30
+
+rem Set the CPU priority to realtime
+wmic process where name="GenshinImpact.exe" CALL setpriority "256"
+
+rem Wait for user input
 pause
+exit /b
+
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "cmd.exe", "/c %~s0 %~1", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+del "%temp%\getadmin.vbs"
+exit /B`
