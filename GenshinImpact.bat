a1dfeb3a2d164ce06cf47d96fead17e5f7ad664a
YuukiRen <reinaldoalvin1@gmail.com>
2020-10-09 14:23:34 +0200

    GenshinImpact: Save the game directory
    When a user runs the script for the first time it will prompt the user to enter the location of the game's executable if the executable is found in that location then the userinput will be saved to gameDirectory.txt in the same directory as the script is ran from.

--- GenshinImpact.bat
+++ GenshinImpact.bat
@@ -1,6 +1,6 @@
 @echo off
 rem Set the title
-title Lacia's GenshinImpact.bat script
+title Lacia's GenshinImpact launcher script
 
 rem Clear the screen of previous commands
 cls
@@ -20,6 +20,36 @@ exit /b
 fsutil dirty query %systemdrive% >nul
 exit /b
 
+
+:getInput
+if not exist "%~dp0"gameDirectory.txt (
+	rem Let user input new genshin game directory if it's their first time
+	echo Example: D:\Program Files\Genshin Impact\Genshin Impact Game
+	set /p gamelocation="Enter the location where the game is installed: "
+) else (
+	rem Take the game directory from saved file
+	set /p gamelocation=<"%~dp0"gameDirectory.txt
+)
+
+:gameExist
+rem Check if genshin game exist in directory
+if not exist "%gamelocation%"\GenshinImpact.exe (
+	cls
+	echo File not found. please recheck directory
+	
+	rem Delete the saved file directory
+	if exist "%~dp0"gameDirectory.txt del "%~dp0"gameDirectory.txt
+
+	rem Get new genshin game directory from user
+	call :getInput
+)
+exit /b
+
+:saveFile
+rem Save genshin game directory in save file
+(echo=%gamelocation%) > "%~dp0"gameDirectory.txt
+exit /b
+
 :run
 rem Tell users what this script will do.
 echo This script will set the graphics quality to its lowest / the fastest preset and it will launch the game
@@ -30,10 +60,10 @@ pause
 rem Clear the screen of previous commands
 cls
 
-rem Let the user enter the location where the game is installed
-echo Example: D:\Program Files\Genshin Impact\Genshin Impact Game
-set /p gamelocation="Enter the location where the game is installed: "
-
+rem Get genshin game directory from user
+call :getInput
+call :gameExist
+call :saveFile
 
 rem Starting game with [-force-d3d11 -screen-width 800 -screen-height 600 -screen-quality lowest] This will run the game on its lowest graphical preset allong with running it in 800x600
 echo Starting the game...
