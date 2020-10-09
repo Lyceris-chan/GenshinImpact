e81c2ebb44aa0fee0039e6014c14b827c82335a8
YuukiRen <reinaldoalvin1@gmail.com>
2020-10-09 18:53:26 +0700

    save genshin directory to gameDirectory.txt in current directory

--- GenshinImpact.bat
+++ GenshinImpact.bat
@@ -20,6 +20,36 @@ exit /b
 fsutil dirty query %systemdrive% >nul
 exit /b
 
+
+:getInput
+if not exist "%~dp0"gameDirectory.txt (
+	rem Let user input new genshin game directory if it's their first time
+	echo Example: D:\Program Files\Genshin Impact\Genshin Impact Game
+	set /p gamelocation="Enter the location where the game is installed: "
+) else (
+	rem Take the game directory from saved file
+	set /p gamelocation=<"%~dp0"gameDirectory.txt
+)
+
+:gameExist
+rem Check if genshin game exist in directory
+if not exist "%gamelocation%"\GenshinImpact.exe (
+	cls
+	echo File not found. please recheck directory
+	
+	rem Delete the saved file directory
+	if exist "%~dp0"gameDirectory.txt del "%~dp0"gameDirectory.txt
+
+	rem Get new genshin game directory from user
+	call :getInput
+)
+exit /b
+
+:saveFile
+rem Save genshin game directory in save file
+(echo=%gamelocation%) > "%~dp0"gameDirectory.txt
+exit /b
+
 :run
 rem Tell users what this script will do.
 echo This script will set the graphics quality to its lowest / the fastest preset and it will launch the game
@@ -29,6 +59,11 @@ pause
 
 rem Clear the screen of previous commands
 cls
+
+rem Get genshin game directory from user
+call :getInput
+call :gameExist
+call :saveFile
 
 rem Let the user enter the location where the game is installed
 echo Example: D:\Program Files\Genshin Impact\Genshin Impact Game
